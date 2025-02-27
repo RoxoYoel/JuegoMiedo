9cac3f49e328fcd140820b8fd0d4c650317c3435
RoxoYoel <yoelvazenero@gmail.com>
2025-02-27 13:21:35 +0100

    inicio

---
+++ Assets/Scripts/Inicio.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Inicio : MonoBehaviour
+{
+    public Volume vl;
+    float suma = 0.05f;
+
+    private void Awake()
+    {
+        Time.timeScale = 0;
+        vl.weight = 0f;
+        StartCoroutine("Ebrio");
+    }
+
+    IEnumerator Ebrio()
+    {
+        for (float i = 0; i < 1; i+=suma)
+        {
+            vl.weight = i;
+            yield return new  WaitForSeconds(0.1f);
+        }
+        StartCoroutine(MenosEbrio());
+    }
+
+    IEnumerator MenosEbrio()
+    {
+        for (float i = 1; i > 0; i -= suma)
+        {
+            vl.weight = i;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    public void Reanudar() 
+    { 
+        Time.timeScale = 1;
+    }
+
+    public void Ocultar(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+
+    public void Mostrar (GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+}
